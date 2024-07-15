743ae9a07006ceba7dc40bf363dfc76fc0ad9c8e
lina192 <pe8708018@gmail.com>
2024-07-14 23:51:53 -0500

    Trabajo1

---
+++ trabajo1/app/src/main/java/co/edu/sena/trabajo1/MainActivity.kt
@@ -0,0 +1,49 @@
+package co.edu.sena.trabajo1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import co.edu.sena.trabajo1.ui.theme.Trabajo1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Trabajo1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Lina Valeria Peña Rincón. \n"+
+                                "Numero de Ficha: 2902093. \n\n",
+
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Trabajo1Theme {
+        Greeting("Android")
+    }
+}
